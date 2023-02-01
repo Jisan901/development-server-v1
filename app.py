bbfe2ae515c2921652bcf95befe63fc65c720edb
Jisan901 <jisuna901@gmail.com>
2023-02-01 21:31:46 +0600

    op fix

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+import dns.resolver
+dns.resolver.default_resolver=dns.resolver.Resolver(configure=False)
+dns.resolver.default_resolver.nameservers=['8.8.8.8']
+
 from app_main import app
 if __name__ == '__main__':
     app.run(debug = False, port = 8000)
